9694369647de72e617e2809746ec75555683ed4d
Ofek Kanari <ofekgki@gmail.com>
2026-01-13 21:54:56 +0200

    finished design - navBar, started design - profileFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/apexracing/ui/profile/ProfileMain.kt
@@ -0,0 +1,6 @@
+package com.example.apexracing.ui.profile
+
+import androidx.fragment.app.Fragment
+class ProfileMain : Fragment() {
+
+}
