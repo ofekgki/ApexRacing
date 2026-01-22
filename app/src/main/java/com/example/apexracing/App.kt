a41612847e640b95fffdbf2b7a87c9f19b125c58
Ofek Kanari <ofekgki@gmail.com>
2026-01-22 12:31:37 +0200

    app handler init

--- app/src/main/java/com/example/apexracing/App.kt
+++ app/src/main/java/com/example/apexracing/App.kt
@@ -1,10 +1,12 @@
 package com.example.apexracing
 
 import android.app.Application
+import com.example.apexracing.api.ApiHandler
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        ApiHandler.init()
     }
 }
